c17d4c221fabfe250d0716b76c03dc723acad668
Ari <106684883+zwei9lake@users.noreply.github.com>
2026-09-06 17:44:17 +0700

    feat: extract links from html

--- src/crawler.py
+++ src/crawler.py
@@ -1,7 +1,21 @@
 import httpx
+from bs4 import BeautifulSoup
 
 def fetch_page(url):
     response = httpx.get(url)
     response.raise_for_status()
 
     return response.text
+
+def extract_links(html):
+    soup = BeautifulSoup(html,"html.parser");
+
+    links = []
+
+    for link in soup.find_all("a"):
+        href = link.get("href")
+
+        if href:
+            links.append(href)
+
+    return links
--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -1,7 +1,23 @@
-from src.crawler import fetch_page
+from src.crawler import fetch_page, extract_links
 
 def test_fetch_page():
     html = fetch_page("https://example.com")
 
     assert "<html" in html.lower()
+
     
+def test_extract_links():
+    html="""
+    <html>
+        <a href="https://example.com">Example</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+    """
+
+    links = extract_links(html)
+
+    assert links == [
+        "https://example.com",
+        "/about",
+        "/contact",
+    ]
