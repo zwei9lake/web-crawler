ccd47b8b753c95a915772d2bf6b1e9a46ed6ecab
Ari <106684883+zwei9lake@users.noreply.github.com>
2026-09-06 21:39:52 +0700

    feat: add multi-page crawling

--- src/crawler.py
+++ src/crawler.py
@@ -25,3 +25,34 @@ def extract_links(html):
 
 def normalize_url(base_url, link):
     return urljoin(base_url,link)
+
+def crawl_page(url):
+    html = fetch_page(url)
+    links = extract_links(html)
+
+    normalize_links = []
+
+    for link in links:
+        normalize_link = normalize_url(url,link)
+        normalize_links.append(normalize_link)
+
+    return normalize_links
+
+def crawl(start_url, max_pages):
+    queue = [start_url]
+    visited = []
+
+    while queue and len (visited) < max_pages:
+        url = queue.pop(0)
+
+        if url in visited:
+            continue
+
+        links = crawl_page(url)
+        visited.append(url)
+
+        for link in links:
+            if link not in visited and link not in queue:
+                queue.append(link)
+
+    return visited
--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -1,4 +1,10 @@
-from src.crawler import fetch_page, extract_links, normalize_url
+from src.crawler import (
+    fetch_page,
+    extract_links,
+    normalize_url,
+    crawl_page,
+    crawl,
+)
 
 def test_fetch_page():
     html = fetch_page("https://example.com")
@@ -49,3 +55,56 @@ def test_normalize_url():
         "./item-1"
     ) == "https://example.com/products/item-1"
     
+
+def test_crawl_patch(monkeypatch):
+    html = """
+    <html>
+        <a href = "/about">About</a>
+        <a href = "products/">Products</a>
+        <a href = "https://google.com>Google</a>
+    </html>
+    """
+
+def mock_fetch_page(url):
+    return html
+
+    monkeypatch.setattr("src.crawler.fetch_page", mock_fetch_page)
+
+    links = crawl_page("https://example.com")
+
+    assert links == [
+        "https://example.com/about",
+        "https://example.com/products",
+        "https://google.com",
+    ]
+
+        
+def test_crawl(monkeypatch):
+    pages = {
+        "https://example.com":"""
+        <a href = "/about">About</a>
+        <a href = "/products">Products</a>
+        """,
+        "https://example.com/about": """
+            <a href="/contact">Contact</a>
+        """,
+        "https://example.com/products": """
+            <a href="/about">About</a>
+        """,
+        "https://example.com/contact": """
+        """,
+    }
+
+    def mock_fetch_page(url):
+        return pages[url]
+
+    monkeypatch.setattr("src.crawler.fetch_page", mock_fetch_page)
+
+    visited = crawl("https://example.com", max_pages=4)
+
+    assert visited == [
+        "https://example.com",
+        "https://example.com/about",
+        "https://example.com/products",
+        "https://example.com/contact",
+    ]
